0e51e8b4368bece39b2d2d9c04353fd76dd88ff1
Endy Muhardin <endy.muhardin@gmail.com>
2016-10-25 09:40:51 +0700

    dummy screen

---
+++ aplikasi-training/src/main/java/com/braimatics/training/controller/PesertaController.java
@@ -0,0 +1,24 @@
+package com.braimatics.training.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/peserta")
+public class PesertaController {
+    
+    @RequestMapping("list")
+    public void daftarMateri(){}
+    
+    @RequestMapping("view")
+    public void detailMateri(){}
+    
+    @RequestMapping(value = "form", method = RequestMethod.GET)
+    public void tampilkanForm(){}
+    
+    @RequestMapping(value = "form", method = RequestMethod.POST)
+    public String prosesForm(){
+        return "redirect:list";
+    }
+}
