f23f745b614277284f9ce86c6d7668fb2e7c5597
Endy Muhardin <endy.muhardin@gmail.com>
2016-10-26 14:43:14 +0700

    implement pagination

--- aplikasi-training/src/main/java/com/braimatics/training/AplikasiTrainingApplication.java
+++ aplikasi-training/src/main/java/com/braimatics/training/AplikasiTrainingApplication.java
@@ -3,7 +3,9 @@ package com.braimatics.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @EntityScan(
         basePackageClasses = {AplikasiTrainingApplication.class, Jsr310JpaConverters.class}
@@ -13,5 +15,10 @@ public class AplikasiTrainingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AplikasiTrainingApplication.class, args);
+    }
+    
+    @Bean
+    public SpringDataDialect springDataDialect() {
+        return new SpringDataDialect();
     }
 }
