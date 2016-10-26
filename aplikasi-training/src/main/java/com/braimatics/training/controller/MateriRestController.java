0e40284585938702d34b6ae836bfd5ba99a1c28b
Endy Muhardin <endy.muhardin@gmail.com>
2016-10-26 16:58:24 +0700

    implement create record via rest

--- aplikasi-training/src/main/java/com/braimatics/training/controller/MateriRestController.java
+++ aplikasi-training/src/main/java/com/braimatics/training/controller/MateriRestController.java
@@ -2,10 +2,15 @@ package com.braimatics.training.controller;
 
 import com.braimatics.training.dao.MateriDao;
 import com.braimatics.training.entity.Materi;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +19,14 @@ public class MateriRestController {
     
     @Autowired private MateriDao md;
     
-    @RequestMapping("/")
+    @RequestMapping(value="/", method = RequestMethod.GET)
     public Page<Materi> daftarMateri(Pageable page){
         return md.findAll(page);
+    }
+    
+    @RequestMapping(value="/", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody @Valid Materi m){
+        md.save(m);
     }
 }
