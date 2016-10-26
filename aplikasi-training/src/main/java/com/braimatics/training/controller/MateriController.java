1235737230a0f0d0755059084436d48a27cc2204
Endy Muhardin <endy.muhardin@gmail.com>
2016-10-26 15:20:13 +0700

    implement display dan proses form

--- aplikasi-training/src/main/java/com/braimatics/training/controller/MateriController.java
+++ aplikasi-training/src/main/java/com/braimatics/training/controller/MateriController.java
@@ -1,12 +1,16 @@
 package com.braimatics.training.controller;
 
 import com.braimatics.training.dao.MateriDao;
+import com.braimatics.training.entity.Materi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/materi")
@@ -27,10 +31,24 @@ public class MateriController {
     public void detailMateri(){}
     
     @RequestMapping(value = "form", method = RequestMethod.GET)
-    public void tampilkanForm(){}
+    public ModelMap tampilkanForm(@RequestParam(required = false) String id){
+        ModelMap mm = new ModelMap();
+        
+        mm.addAttribute("materi", new Materi());
+        
+        if(StringUtils.hasText(id)){
+            Materi m = md.findOne(id);
+            if(m != null){
+                mm.addAttribute("materi", m);
+            }
+        }
+        
+        return mm;
+    }
     
     @RequestMapping(value = "form", method = RequestMethod.POST)
-    public String prosesForm(){
+    public String prosesForm(@ModelAttribute Materi m){
+        md.save(m);
         return "redirect:list";
     }
 }
