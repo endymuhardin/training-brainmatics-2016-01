e6b85bea126f343786696d4a99653bf8244680f1
Endy Muhardin <endy.muhardin@gmail.com>
2016-10-27 12:04:12 +0700

    tampilkan form edit kelas

--- aplikasi-training/src/main/java/com/braimatics/training/controller/KelasController.java
+++ aplikasi-training/src/main/java/com/braimatics/training/controller/KelasController.java
@@ -1,13 +1,21 @@
 package com.braimatics.training.controller;
 
 import com.braimatics.training.dao.KelasDao;
+import com.braimatics.training.entity.Kelas;
+import com.braimatics.training.entity.Materi;
+import com.braimatics.training.entity.Peserta;
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+
+@SessionAttributes(names = {"daftarMateri", "daftarPeserta"})
 @Controller
 @RequestMapping("/kelas")
 public class KelasController {
@@ -15,15 +23,25 @@ public class KelasController {
     @Autowired private KelasDao kd;
     
     @RequestMapping("list")
-    public void daftarMateri(Pageable page, ModelMap mm){
+    public void daftarKelas(Pageable page, ModelMap mm){
         mm.addAttribute("daftarKelas", kd.findAll(page));
     }
     
     @RequestMapping("view")
-    public void detailMateri(){}
+    public void detailKelas(){}
     
     @RequestMapping(value = "form", method = RequestMethod.GET)
-    public void tampilkanForm(){}
+    public void tampilkanForm(@RequestParam(name = "id", required = false) Kelas kelas, ModelMap mm){
+        if(kelas != null){
+            mm.addAttribute("kelas", kelas);
+            mm.addAttribute("daftarMateri", kelas.getDaftarMateri());
+            mm.addAttribute("daftarPeserta", kelas.getDaftarPeserta());
+        } else {
+            mm.addAttribute("kelas", new Kelas());
+            mm.addAttribute("daftarMateri", new ArrayList<Materi>());
+            mm.addAttribute("daftarPeserta", new ArrayList<Peserta>());
+        }
+    }
     
     @RequestMapping(value = "form", method = RequestMethod.POST)
     public String prosesForm(){
