cda47adebeae712b9d3feb328b3b723640222793
Endy Muhardin <endy.muhardin@gmail.com>
2016-10-27 09:33:54 +0700

    implement daftar peserta

--- aplikasi-training/src/main/java/com/braimatics/training/controller/PesertaController.java
+++ aplikasi-training/src/main/java/com/braimatics/training/controller/PesertaController.java
@@ -1,6 +1,12 @@
 package com.braimatics.training.controller;
 
+import com.braimatics.training.dao.PesertaDao;
+import com.braimatics.training.entity.Peserta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,8 +14,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/peserta")
 public class PesertaController {
     
+    @Autowired private PesertaDao pd;
+    
     @RequestMapping("list")
-    public void daftarMateri(){}
+    public void daftarMateri(ModelMap mm, Pageable page){
+        Page<Peserta> data = pd.findAll(page);
+        mm.addAttribute("daftarPeserta", data);
+    }
     
     @RequestMapping("view")
     public void detailMateri(){}
