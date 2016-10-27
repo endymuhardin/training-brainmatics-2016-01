778bdc501c3a7ee4a00d2a49c70d02089555d517
Endy Muhardin <endy.muhardin@gmail.com>
2016-10-27 11:46:48 +0700

    implement daftar kelas

--- aplikasi-training/src/main/java/com/braimatics/training/controller/KelasController.java
+++ aplikasi-training/src/main/java/com/braimatics/training/controller/KelasController.java
@@ -1,6 +1,10 @@
 package com.braimatics.training.controller;
 
+import com.braimatics.training.dao.KelasDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,8 +12,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/kelas")
 public class KelasController {
     
+    @Autowired private KelasDao kd;
+    
     @RequestMapping("list")
-    public void daftarMateri(){}
+    public void daftarMateri(Pageable page, ModelMap mm){
+        mm.addAttribute("daftarKelas", kd.findAll(page));
+    }
     
     @RequestMapping("view")
     public void detailMateri(){}
